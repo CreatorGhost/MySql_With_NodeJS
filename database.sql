efc1f83398ed90f857a92d27cd41c47f3979b209
Aditya Pratap Singh <adityapratapsingh@Adityas-MacBook-Air.local>
2021-12-05 16:54:22 +0530

    Updated

--- database.sql
+++ database.sql
@@ -6,5 +6,6 @@ CREATE TABLE Persons (
 );
 Insert into Persons values (1, 'John', 25);
 Insert into Persons values (2, 'Mary', 30);
-
-
+Insert into Persons values (3, 'Mike', 35);
+Insert into Persons values (4, 'Bob', 40);
+Insert into Persons values (5, 'Sam', 45);
